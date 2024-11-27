d6e311120f5a1f8eaf154e784e5fc0f288918510
mdeniz <moisesdenizaleman@gmail.com>
2024-11-27 23:43:36 +0000

    Add breadcrums

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -2,16 +2,20 @@ class ShopController < ApplicationController
   before_action :set_menu
 
   def homepage
+    @breadcrums = [ ]
     @products = Product.order("RANDOM()").limit(4)
   end
 
   def catalog
+    @breadcrums = [ ]
   end
 
   def product
+    @breadcrums = [ ]
   end
 
   private
+
     # Calculate the categories in the menu
     def set_menu
       @menu = Category.where(parent: nil).order(:order)
