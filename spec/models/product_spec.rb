a298046b57fb661babb1b2b1604262d12202498a
mdeniz <moisesdenizaleman@gmail.com>
2024-11-27 14:01:33 +0000

    Complete model specs for Product

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,52 +1,114 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  subject(:product) { build(:product) }
+  describe "validations" do
+    subject(:product) { build(:product) }
 
-  it { is_expected.to be_valid }
+    it { is_expected.to be_valid }
 
-  context 'whit brand not present' do
-    subject(:product) { build(:product, brand: "") }
+    context 'whit brand not present' do
+      subject(:product) { build(:product, brand: "") }
 
-    it { is_expected.not_to be_valid }
+      it { is_expected.not_to be_valid }
+    end
+
+    context 'whit model not present' do
+      subject(:product) { build(:product, model: "") }
+
+      it { is_expected.not_to be_valid }
+    end
+
+    context 'whit description not present' do
+      subject(:product) { build(:product, description: "") }
+
+      it { is_expected.not_to be_valid }
+    end
+
+    context 'whit standalone price not present' do
+      subject(:product) { build(:product, standalone_price: nil) }
+
+      it { is_expected.not_to be_valid }
+    end
+
+    context 'whit negative standalone price' do
+      subject(:product) { build(:product, standalone_price: -1) }
+
+      it { is_expected.not_to be_valid }
+    end
+
+    context 'whit standalone price wrongly formatted' do
+      subject(:product) { build(:product, standalone_price: "bad number") }
+
+      it { is_expected.not_to be_valid }
+    end
+
+    context 'whit uuid not present it generates one' do
+      let(:uuid_regex) { /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/ }
+
+      subject(:product) { create(:product, uuid: nil) }
+
+      it { expect(product.uuid).not_to be_nil }
+      it { expect(product.uuid).to match(uuid_regex) }
+    end
   end
 
-  context 'whit model not present' do
-    subject(:product) { build(:product, model: "") }
+  describe "#incompatible_products" do
+    let!(:product) { create(:product) }
+    let!(:another_product) { create(:product) }
 
-    it { is_expected.not_to be_valid }
+    subject(:incompatible_products) { product.incompatible_products }
+
+    context "with banned combination as source" do
+      let!(:banned_combinations) { create(:banned_combination, source: product, target: another_product) }
+
+      it { expect(incompatible_products).to include(another_product) }
+    end
+
+    context "with banned combination as target" do
+      let!(:banned_combinations) { create(:banned_combination, source: another_product, target: product) }
+
+      it { expect(incompatible_products).to include(another_product) }
+    end
   end
 
-  context 'whit description not present' do
-    subject(:product) { build(:product, description: "") }
+  describe "#incompatible_products_ids" do
+    let!(:product) { create(:product) }
+    let!(:another_product) { create(:product) }
 
-    it { is_expected.not_to be_valid }
+    subject(:incompatible_products_ids) { product.incompatible_products_ids }
+
+    context "with banned combination as source" do
+      let!(:banned_combinations) { create(:banned_combination, source: product, target: another_product) }
+
+      it { expect(incompatible_products_ids).to include(another_product.id) }
+    end
+
+    context "with banned combination as target" do
+      let!(:banned_combinations) { create(:banned_combination, source: another_product, target: product) }
+
+      it { expect(incompatible_products_ids).to include(another_product.id) }
+    end
   end
 
-  context 'whit standalone price not present' do
-    subject(:product) { build(:product, standalone_price: nil) }
+  describe "#selection_price" do
+    let!(:product) { create(:product, standalone_price: 10) }
+    let!(:another_product) { create(:product, standalone_price: 5) }
+    let(:selected_products_ids) { [ product.id, another_product.id ] }
+    let(:another_product_selection_price) { another_product.selection_price(selected_products_ids) }
 
-    it { is_expected.not_to be_valid }
-  end
+    subject(:product_selection_price) { product.selection_price(selected_products_ids) }
 
-  context 'whit negative standalone price' do
-    subject(:product) { build(:product, standalone_price: -1) }
+    context "without price changes" do
+      it { is_expected.to eq(15) }
+    end
 
-    it { is_expected.not_to be_valid }
-  end
+    context "with price changes" do
+      let!(:product_not_in_selection) { create(:product, standalone_price: 1) }
+      let!(:ignorable_price_change) { create(:price_change, changed_product: product, on_product: product_not_in_selection, change: 700) }
+      let!(:price_change_in_selection) { create(:price_change, changed_product: product, on_product: another_product, change: 3) }
 
-  context 'whit standalone price wrongly formatted' do
-    subject(:product) { build(:product, standalone_price: "bad number") }
-
-    it { is_expected.not_to be_valid }
-  end
-
-  context 'whit uuid not present it generates one' do
-    let(:uuid_regex) { /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/ }
-
-    subject(:product) { create(:product, uuid: nil) }
-
-    it { expect(product.uuid).not_to be_nil }
-    it { expect(product.uuid).to match(uuid_regex) }
+      it { is_expected.to eq(18) }
+      it { expect(another_product_selection_price).to eq(18) }
+    end
   end
 end
