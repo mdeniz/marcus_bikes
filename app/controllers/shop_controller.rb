88d614e6526ce65ebf3453ae887eb1eef78cbeb4
mdeniz <moisesdenizaleman@gmail.com>
2024-11-28 01:47:18 +0000

    Add catalog view

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -8,6 +8,12 @@ class ShopController < ApplicationController
 
   def catalog
     @breadcrums = []
+    if params[:category].present?
+      category_ids = Category.find(params[:category]).self_and_descendants.map(&:id)
+      @products = Product.where(category: category_ids)
+    else
+      @products = Product.all
+    end
   end
 
   def product
