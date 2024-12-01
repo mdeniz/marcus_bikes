080114515595fe3d95b9367a1a2de4a37f083b1b
mdeniz <moisesdenizaleman@gmail.com>
2024-12-01 19:57:54 +0000

    Improve currency format. Improve cart show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
 module ApplicationHelper
   include Pagy::Frontend
+
+  def number_to_euros(number)
+    number_to_currency(number, separator: ",", delimiter: ".",  unit: "€", format: "%n %u")
+  end
 end
